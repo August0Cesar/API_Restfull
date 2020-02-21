0839cd39b755686294b50e9ee5ee0fb7468183c7
Augusto Matos <augusto_matos@qat.com>
2020-02-21 17:33:31 -0300

    [APIRestfull] A: adicionado mudança no response quando der exceção

--- src/main/java/br/com/augusto/api_teste/exceptions/BaseException.java
+++ src/main/java/br/com/augusto/api_teste/exceptions/BaseException.java
@@ -1,5 +1,7 @@
 package br.com.augusto.api_teste.exceptions;
 
+import java.util.Arrays;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import br.com.augusto.api_teste.model.ApiError;
+
 @ControllerAdvice
 public class BaseException extends ResponseEntityExceptionHandler {
 	private static final Logger LOGGER = LogManager.getLogger(BaseException.class);
@@ -16,9 +20,12 @@ public class BaseException extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
-		// TODO Auto-generated method stub
 		
-		LOGGER.info("Exception encontrado.");
-		return super.handleExceptionInternal(ex, body, headers, status, request);
+		LOGGER.warn("Nova exception ...");
+		
+		ApiError apiError = new ApiError(status, "Missing Request", Arrays.asList(ex.getMessage()));
+		
+		return new ResponseEntity<>(apiError,
+				new HttpHeaders(), apiError.getStatus());
 	}
 }
--- src/main/java/br/com/augusto/api_teste/model/ApiError.java
+++ src/main/java/br/com/augusto/api_teste/model/ApiError.java
@@ -6,13 +6,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 
 public class ApiError {
-	@SuppressWarnings("unused") 
-    private HttpStatus status;
-    
-    @SuppressWarnings("unused")
-	private String message;
-    
-    @SuppressWarnings("unused")
+	private HttpStatus status;
+    private String message;
     private List<String> errors;
  
     public ApiError(HttpStatus status, String message, List<String> errors) {
@@ -28,4 +23,29 @@ public class ApiError {
         this.message = message;
         errors = Arrays.asList(error);
     }
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+    
 }
